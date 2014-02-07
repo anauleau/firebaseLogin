137746e8eb81abfcbde8ef8dc95e4f0c0f19c21e
Andreas Nauleau <andreas@adap.tv>
2014-02-07 11:55:50 -0800

    Replaces bootstrap with foundation

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = angular.module('app', ['firebase']);
 
 app.controller('main', function ($scope, $firebase) {
-    var ref = new Firebase("https://cukvje20bt1.firebaseio-demo.com/");
+    var ref = new Firebase("https://torid-fire-9175.firebaseio.com/");
     $scope.messages = $firebase(ref);
     $scope.addMessage = function(e) {
       if (e.keyCode != 13) return;
