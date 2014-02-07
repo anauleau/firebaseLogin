c6727a9b3d788882272aaf648f6e1a46fbf63368
Andreas Nauleau <andreas@adap.tv>
2014-02-07 11:36:50 -0800

    Adds angularfire

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['firebase']);
 
-app.controller('main', function ($scope, $q) {
-    
+app.controller('main', function ($scope, $firebase) {
+    var ref = new Firebase("https://cukvje20bt1.firebaseio-demo.com/");
+    $scope.messages = $firebase(ref);
+    $scope.addMessage = function(e) {
+      if (e.keyCode != 13) return;
+      $scope.time = moment().format('hmmss');
+      $scope.messages.$add({from: $scope.name, body: $scope.msg, time: $scope.time});
+      $scope.msg = "";
+    };
 });
