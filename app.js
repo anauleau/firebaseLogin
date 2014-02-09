b567b82185e620063eba0820d762fa6aad671992
Andreas Nauleau <andreas@adap.tv>
2014-02-09 14:17:24 -0800

    adds working simple login code

--- app.js
+++ app.js
@@ -3,21 +3,19 @@ var app = angular.module('app', [
     'ngRoute'
 ]);
 
-app.controller('main', function ($scope, $firebase) {
+app.controller('main', function ($scope, $firebase, $firebaseSimpleLogin) {
+    var url = new Firebase("https://torid-fire-9175.firebaseio.com/");
+    $scope.loginObj = $firebaseSimpleLogin(url);
+
+    $scope.login = function () {
+        $scope.loginObj.$login('password', {
+           email: $scope.email,
+           password: $scope.password
+        }).then(function(user) {
+           console.log('Logged in as: ', user.uid);
+           console.log(user);
+        }, function(error) {
+           console.error('Login failed: ', error);
+        });
+    }
 });
-
-app.service('myAuthService', ["$rootScope", function($rootScope) {
-    var ref = new Firebase("https://torid-fire-9175.firebaseio.com/");
-
-    this.auth = new FirebaseAuthClient(ref, function(error, user) {
-        if (user) {
-            $rootScope.$emit("login", user);
-        }
-        else if (error) {
-            $rootScope.$emit("loginError", error);
-        }
-        else {
-            $rootScope.$emit("logout");
-        }
-    });
-}])
